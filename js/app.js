0c51e1b508119e811c6323013e0615a6a5c7fc54
Bryan Alexander <bryanalexander@me.com>
2015-09-25 18:51:36 -1000

    completed ~75% of visualization structure using HTML5 Templates and custom elements. creating that structure was more intense than I anticipated, but I've learned a lot and the finished product will be worth it. focusing on the other sorting methodsnow and will come back to this later.

--- js/app.js
+++ js/app.js
@@ -2,74 +2,161 @@ var sortModule = (function() {
 
   var module = {
 
-    setTemplate : _setTemplate,
-    setStage : _setStage,
+    setSortTypeButtonTemplate : _setSortTypeButtonTemplate,
+    setSortSectionTemplate : _setSortSectionTemplate,
+    setOpeningStage : _setOpeningStage,
+    setSectionStage : _setSectionStage,
     bubbleSort : _bubbleSort,
     quickSort : _quickSort,
     init : _init
 
   };
 
-  function _setTemplate(sortName) {
+  function _setSortTypeButtonTemplate() {
 
-    //define template for DRY rendering of HTML elements
+    //define button template for DRY rendering of HTML button elements
 
-    var sortTemplate = document.createElement('template');
-    document.head.appendChild(sortTemplate);
-    sortTemplate.id = 'sortTemplate';
+    var sortTypeButtonTemplate = document.createElement('template');
+    document.head.appendChild(sortTypeButtonTemplate);
+    sortTypeButtonTemplate.id = 'sortTypeButtonTemplate';
 
-    //define container in template
+    //define new HTML element sort-type-button
 
-    var sortContainer = document.createElement('section');
-    sortTemplate.appendChild(sortContainer);
-    sortContainer.id = sortName + 'SortContainer';
-    sortContainer.className = 'sortContainer';
+    var SortTypeButton = Object.create(HTMLElement.prototype);
+    document.registerElement('sort-type-button');
 
-    //section heading in template
+    //instantiate the sort-type-button elements on the page for the length of the array and create the innerHTML from the array
 
-    var sortHeading = document.createElement('h1');
-    sortContainer.appendChild(sortHeading);
-    sortHeading.id = sortName + 'SortHeading';
-    sortHeading.className = 'sortHeading';
-    sortHeading.innerHTML = sortName + ' Sort Visualization';
+    var sortTypeButton = document.createElement('sort-type-button');
+    sortTypeButtonTemplate.appendChild(sortTypeButton);
+    sortTypeButton.className = 'sortTypeButton';
 
-    //define new HTML element[sortName]sort-block
+  }
 
-    var SortBlock = Object.create(HTMLElement.prototype);
-    document.registerElement( sortName + 'sort-block');
+  function _setSortSectionTemplate() {
 
-    //instantiate the sort-block elements on the page for the length of the array and create the block text node from the array
+    // //define template for DRY rendering of HTML sort elements
+
+    // var sortSectionTemplate = document.createElement('template');
+    // document.head.appendChild(sortSectionTemplate);
+    // sortSectionTemplate.id = 'sortSectionTemplate';
+
+    // //define container in template
+
+    // var sortContainer = document.createElement('section');
+    // sortSectionTemplate.appendChild(sortContainer);
+    // sortContainer.className = 'sortContainer';
+    // sortContainer.id = sortName + 'SortContainer';
+
+    // //section heading in template
+
+    // var sortHeading = document.createElement('h1');
+    // sortContainer.appendChild(sortHeading);
+    // sortHeading.className = 'sortHeading';
+    // sortHeading.id = sortName + 'SortHeading';
+    // sortHeading.innerHTML = sortName + ' Sort Visualization';
+
+    // //define new HTML element[sortName]sort-block
+
+    // var SortBlock = Object.create(HTMLElement.prototype);
+    // document.registerElement( sortName + 'sort-block');
+
+    //instantiate the sort-block elements on the page for the length of the array and create the innerHTML from the array
+
+    // for (var i = 0; i < arr.length; i++) {
+
+    //   var sortBlock = document.createElement( sortName + 'sort-block');
+    //   sortContainer.appendChild(SortBlock);
+    //   sortBlock.dataset.id = i;
+    //   sortBlock.className = sortName + 'sortBlock';
+    //   sortBlock.innerHTML = arr[i];
+
+    // }
+
+  }
+
+  //sets the opening stage with header
+
+  function _setOpeningStage(arr) {
+
+    //add header and h elements & populate innerHTML
+    var pageHeader = document.createElement('header');
+    document.body.appendChild(pageHeader);
+
+    var pageHeading = document.createElement('h1');
+    pageHeader.appendChild(pageHeading);
+    pageHeading.id = 'titleHeading';
+    pageHeading.innerHTML = 'Sort Visualization';
+
+    var pageSubHeading = document.createElement('h2');
+    pageHeader.appendChild(pageSubHeading);
+    pageSubHeading.id = 'subTitleHeading';
+    pageSubHeading.innerHTML = 'Pick a Sort Method! (or more than one to compare)';
+
+    //grab the sortTypeButtonTemplate & clone the contents
+
+    var template = document.querySelector('#sortTypeButtonTemplate');
+
+    //clone the contents of the template before placing on the DOM
+
+    var templateClone = document.importNode(template.content, true);
+
+    //create five unique clones of the sortTypeButtonTemplate within the Template
 
     for (var i = 0; i < arr.length; i++) {
 
-      var sortBlock = document.createElement( sortName + 'sort-block');
-      sortContainer.appendChild(SortBlock);
-      sortBlock.dataset.id = i;
-      sortBlock.className = sortName + 'sortBlock';
-      sortBlock.innerHTML = arr[i];
+      templateClone.querySelector('.sortTypeButton').dataset.id = arr[i];
+      var sortTypeButtonClone = templateClone.querySelector('.sortTypeButton');
+      //sortTypeButtonClone.dataset.id = arr[i];
+      sortTypeButtonClone.textContent = arr[i];
+      sortTypeButtonClone.id = arr[i] + 'SortTypeButton';
+      sortTypeButtonClone.innerHTML = arr[i];
+      sortTypeButtonClone.addEventListener('click', function() {
+
+        _setMainStage(arr[i]);
+
+      });
+
+      document.body.appendChild(templateClone);
 
     }
 
-  }
-
-  function _setStage(sortName) {
-
-
-    //button for end-user to start merge
-
-    var bubbleSortStartButton = document.createElement('button');
-    bubbleSortContainer.appendChild(bubbleSortStartButton);
-    bubbleSortStartButton.id = 'bubbleSortStartButton';
-    bubbleSortStartButton.innerHTML = 'Sort';
-    bubbleSortStartButton.addEventListener('click', _bubbleSort);
+    document.body.appendChild(templateClone);
 
   }
+
+  function _setSectionStage(sortName) {
+
+
+
+
+    //button for end-user to start sort
+
+    var sortStartButton = document.createElement('button');
+    sortContainer.appendChild(bubbleSortStartButton);
+    sortStartButton.id = 'bubbleSortStartButton';
+    sortStartButton.innerHTML = 'Sort';
+    sortStartButton.addEventListener('click', _bubbleSort);
+
+    //button for end-user to start compare
+
+    var compareStartButton = document.createElement('button');
+    sortContainer.appendChild(bubbleSortStartButton);
+    sortStartButton.id = 'bubbleSortStartButton';
+    sortStartButton.innerHTML = 'Sort';
+    sortStartButton.addEventListener('click', _bubbleSort);
+
+  }
+
+  //init function populates the template content and starts the process of setting the stage
 
   function _init(sortName) {
 
-    _setTemplate(sortName);
+    _setSortTypeButtonTemplate();
 
-    _setStage(sortName);
+    _setSortSectionTemplate();
+
+    _setOpeningStage(['bubble', 'quick', 'merge', 'insertion', 'selection']);
 
   }
 
@@ -79,6 +166,6 @@ var sortModule = (function() {
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  sortModule.init(sortName);
+  sortModule.init();
 
 });
--- js/quickSort.js
+++ js/quickSort.js
@@ -1,66 +1,19 @@
-function _quickSort() {
+var arr = [5,4,2,9,8,7,1,5,3,6];
 
-  //the swapped var will let be used to know when the sort is done
 
-  var swapped;
 
-  //wrap the for loop in an interval so the movement of nodes is visible as the sort progresses
+function quickSort(arr, lo, hi) {
 
-  var toStop = setInterval(function() {
 
-    swapped = false;
 
-    //comparison nodes to enable removal and insertion on the DOM
 
-    var currentNode;
-    var nodeAtNext;
-    var nodeToMove;
 
-    for (var i = 0; i < arr.length; i++) {
 
-      if (arr[i] > arr[i + 1]) {
 
-        //if the current value is greater than the next set a temp variable with the current value
 
-        var temp = arr[i];
-
-        //similar to above set the current node and next node to enable DOM removal / insertion
-
-        currentNode = document.querySelector('bubblesort-block[data-id="' + i + '"]');
-        nodeAtNext = document.querySelector('bubblesort-block[data-id="' + [i + 1] + '"]');
-
-        //assign current value to the next index position
-
-        arr[i] = arr[i + 1];
-
-        //remove the nextNode from the DOM and reset the data-id attribut to the correct index position
-
-        nodeToMove = bubbleSortContainer.removeChild(nodeAtNext);
-        nodeToMove.setAttribute('data-id', i);
-
-        //insert the removed node back into the DOM before the currentNode and set the currentNode's data-id to the nextNode
-
-        bubbleSortContainer.insertBefore(nodeToMove, currentNode);
-        currentNode.setAttribute('data-id', i + 1);
-
-        //re-assign the temp variable to the next array position
-
-        arr[i + 1] = temp;
-
-        swapped = true;
-
-      }
-
-    }
-
-    if (!swapped) {
-
-      clearInterval(toStop);
-
-    }
-
-  }, 500);
-
+  console.log(arr);
   return arr;
 
 }
+
+quickSort();
