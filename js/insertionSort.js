ad1abe2915e4664c2efd28b8939210ef5d520c04
Bryan Alexander <bryanalexander@me.com>
2015-09-27 10:36:54 -1000

    completed insertionSort. comments added.

---
+++ js/insertionSort.js
@@ -0,0 +1,35 @@
+var arr = [5,4,2,9,8,7,3,6,1];
+
+function _insertionSort(arr) {
+
+  var value,                    // the value currently being compared
+      i,                        // index into unsorted section
+      j;                        // index into sorted section
+
+  for (i = 0; i < arr.length; i++) {
+
+    // store the current value because it may shift later
+
+    value = arr[i];
+
+    /*
+     * Whenever the value in the sorted section is greater than the value
+     * in the unsorted section, shift all items in the sorted section over
+     * by one. This creates space in which to insert the value.
+     */
+
+    for (j = i - 1; j > -1 && arr[j] > value; j--) {
+
+      arr[j + 1] = arr[j];
+
+    }
+
+    arr[j + 1] = value;
+
+  }
+
+  return arr;
+
+}
+
+console.log(_insertionSort(arr));
